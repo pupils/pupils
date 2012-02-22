5d6295c3c1869687e8a0a6560de386bbf16a9579
franlu <franlu@58014b41-5bfd-43f5-a4be-6ab8f3083e9b>
2012-02-22 22:25:24 +0000

    [UP] Datos de contacto de admin

--- pupils/settings.py
+++ pupils/settings.py
@@ -4,7 +4,8 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Serfin Velez', 'serafin@pupils.es'),
+    ('Fran Lucena','fran@pupils.es'),
 )
 
 MANAGERS = ADMINS

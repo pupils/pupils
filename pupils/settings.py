a86064e9fc49617b184f11f798416ac3c28177f0
franlu <franlu@58014b41-5bfd-43f5-a4be-6ab8f3083e9b>
2012-02-28 18:44:29 +0000

    [UP] template path

--- pupils/settings.py
+++ pupils/settings.py
@@ -1,4 +1,5 @@
 # Django settings for pupils project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -103,11 +104,14 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'pupils.urls'
 
+my_dir = os.popen("pwd", "r").read() 
+THIS_DIR = my_dir[0:(len(my_dir)-1)]
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/pupils/templates"
+    THIS_DIR + '/templates',
 )
 
 INSTALLED_APPS = (

89ae6991b4167d72177942b6b51b15d2192551d8
franlu <franlu@58014b41-5bfd-43f5-a4be-6ab8f3083e9b>
2012-03-01 09:14:00 +0000

    [ADD] Settings for media files

--- pupils/urls.py
+++ pupils/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-
+from pupils.settings import MEDIA_ROOT
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -14,4 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    
+    #Media
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': MEDIA_ROOT}),
 )

e9e30ab127591ccb6c0a5002f0331973a6addc9e
marinatorres <marinatorres@58014b41-5bfd-43f5-a4be-6ab8f3083e9b>
2012-03-02 16:26:35 +0000

    add coding ficheros

--- pupils/views.py
+++ pupils/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Copyright (C) 2012 Fco. Javier Lucena Lucena (https://forja.rediris.es/users/franlu/)
 Copyright (C) 2012 Serafín Vélez Barrera (https://forja.rediris.es/users/seravb/)

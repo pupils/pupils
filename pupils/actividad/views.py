25893682bbab5aa22e4d977ac919b5f82830271f
franlu <franlu@58014b41-5bfd-43f5-a4be-6ab8f3083e9b>
2012-03-12 22:34:55 +0000

    [ADD] Vista de detalle para cada actividad

--- pupils/actividad/views.py
+++ pupils/actividad/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+from pupils.actividad.models import Actividad
+
+def detalle(request,idactividad):
+	
+	try:
+	    act = Actividad.objects.get(id=idactividad)
+	except:
+		act = False
+	
+	context = {'actividad' : act,}
+	
+	return render_to_response('actividad/actividad_detalle.html',context,context_instance=RequestContext(request))

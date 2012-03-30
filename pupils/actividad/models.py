b2e9adf54946b390ee20e23f26581deabfc494ad
franlu <franlu@58014b41-5bfd-43f5-a4be-6ab8f3083e9b>
2012-03-30 01:32:19 +0000

    [IMP] Limpieza de código

--- pupils/actividad/models.py
+++ pupils/actividad/models.py
@@ -1,26 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 import datetime
-## titulo, lugar, fecha, observaciones, pùblico, rango de edades, 
-##
-##
-
-# Create your models here.
 
 class Actividad(models.Model):
-	"""
-		Campos del modelo actividad de django - clase Actividad
-	"""
-	#GENDER_CHOICES_EQUIPMENT = (
-	#	('L', 'Linterna'),
-	#	('C', 'Cantimplora'),
-	#)
-	title = models.TextField() # Titulo
-	descripcion = models.TextField() # Descripcion de la actividad
-	place = models.TextField() # Lugar
-	date_start = models.DateTimeField() #Fecha inicio y fin
+
+	title = models.TextField() 
+	descripcion = models.TextField() 
+	place = models.TextField() 
+	date_start = models.DateTimeField() 
 	date_end = models.DateTimeField()
-	timetable = models.TextField() # Horarios y otras explicaciones (p.e. la actividad es solo martes y jueves de 8 a 15 horas.)
-	age_range = models.CharField(max_length='100') # Rango de edades
-	equipment = models.TextField() # Equipamiento para la actividad	
-	notes = models.TextField() # Observaciones
+	timetable = models.TextField() 
+	age_range = models.CharField(max_length='100') 
+	equipment = models.TextField() 
+	notes = models.TextField()

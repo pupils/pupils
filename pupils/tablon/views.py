9617bbae608fd0ca695578f22414de7678c5468f
alvarohurtado84 <alvarohurtado84@58014b41-5bfd-43f5-a4be-6ab8f3083e9b>
2012-04-13 12:12:16 +0000

    BD actualizada

--- pupils/tablon/views.py
+++ pupils/tablon/views.py
@@ -10,14 +10,21 @@ from tablon.forms import PostHijosForm
 @login_required
 def nuevo_post_hijo(request):
 
-    form = PostHijosForm()
-    context= {
-        "form" : form,
-    }
-
-    return render_to_response('tablon/form_hijo.html',
-                              context,
-                              context_instance=RequestContext(request))
+    if request.method == "POST":
+        form = PostHijosForm(request)
+        if form.is_valid():
+            pass
+        else:
+            pass
+    else:
+        form = PostHijosForm()
+        context= {
+            "form" : form,
+        }
+    
+        return render_to_response('tablon/form_hijo.html',
+                                  context,
+                                  context_instance=RequestContext(request))
     
     
     

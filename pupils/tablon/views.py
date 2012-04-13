7b362dcda15a1bd31fa69fc25953e00740e9c7c4
alvarohurtado84 <alvarohurtado84@58014b41-5bfd-43f5-a4be-6ab8f3083e9b>
2012-04-13 11:29:51 +0000

    Añadida url para tablon.

--- pupils/tablon/views.py
+++ pupils/tablon/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+from django.contrib.auth.decorators import login_required
+
+from tablon.forms import PostHijosForm
+
+@login_required
+def nuevo_post_hijo(request):
+
+    form = PostHijosForm()
+    context= {}
+
+    return render_to_response('tablon/form_hijo.html',
+                              context,
+                              context_instance=RequestContext(request))

9566962c46558d68d2a4b95cc1838113baa11dfa
alvarohurtado84 <alvarohurtado84@58014b41-5bfd-43f5-a4be-6ab8f3083e9b>
2012-04-13 16:57:12 +0000

    Mas cambios

--- pupils/tablon/forms.py
+++ pupils/tablon/forms.py
@@ -6,3 +6,22 @@ class PostHijosForm(ModelForm):
     class Meta:
         model = Post
         exclude = ('hijo', 'published_by')
+        
+    def save(self, hijos, publisher, **kwargs):
+        instance = super(PostHijosForm, self).save(commit=False)
+        
+        if len(hijos) > 0:
+            for hijo in hijos:
+                instance.hijo = hijo
+                instance.published_by = publisher
+                done = instance.save()
+                instance.id = none
+        else:
+            instance.published_by = publisher
+            
+            #siguiente l’nea es una CHAPUZA
+            instance.hijo_id = 0
+            done = instance.save()
+        
+        return done
+
--- pupils/tablon/views.py
+++ pupils/tablon/views.py
@@ -7,6 +7,7 @@ from django.template.context import RequestContext
 from django.contrib.auth.decorators import login_required
 
 from tablon.forms import PostHijosForm
+from users.models import Hijo
 
 @login_required
 def nuevo_post_hijo(request):
@@ -15,13 +16,21 @@ def nuevo_post_hijo(request):
         
         form = PostHijosForm(request.POST)
         if form.is_valid():
-            form.save()
+            try:
+                hijos = []
+                hijos.append(request.user.hijo)
+            except Hijo.DoesNotExist:
+                print "*"*20
+                print "No es un hijo"
+                print "*"*20
+            #print form.save()
+            print form.save(hijos=hijos, publisher=request.user)
             return HttpResponse("Hecho correctamente.")
                 
         else:
             return HttpResponse("No realizado.")
-    else:
-        
+    
+    else:    
         form = PostHijosForm()
         context= {
             "form" : form,

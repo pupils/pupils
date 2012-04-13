3f9192a4a837fe2516bfacf07a98cb60bcb82eae
andresduque <andresduque@58014b41-5bfd-43f5-a4be-6ab8f3083e9b>
2012-04-13 11:25:23 +0000

    urls modificada upload pdf

--- pupils/urls.py
+++ pupils/urls.py
@@ -30,6 +30,7 @@ urlpatterns = patterns('',
     url(r'^usuario/logout/$', 'django.contrib.auth.views.logout',{'next_page': '/'}, name="logout"),
     url(r'^usuario/', include('registration.backends.default.urls')),
     url(r'^pcontrol/', 'pupils.users.views.panel', name="panel"),
+    url(r'^pcontrol/uploadpdf/', 'pupils.users.views.upload_pdf', name="panel"),
     url(r'^actividad/(?P<idactividad>\d+)/$', 'pupils.actividad.views.detalle', name="actividad_detalle"),
     url(r'^$', 'pupils.views.home', name='home'),
     
--- pupils/users/views.py
+++ pupils/users/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from django.contrib.auth.decorators import login_required
+from django import forms
 
 
 @login_required
@@ -16,3 +17,18 @@ def panel(request):
 	return render_to_response('users/panel_control_padre.html',
 							  context,
 							  context_instance=RequestContext(request))
+
+class UploadFileFrom (forms.Form):
+	title = form.CharField(max_length=50)
+	file = form.FileField()
+
+def upload_pdf (request):
+	
+	if request.method == 'POST' :
+		form = UploadFile(request.POST, request.FILES)
+		if form.is_valid():
+			archivo_por_subir(request.FILES['file']
+			return HttpResponseRedirect('Subida hecha con exito')
+		else:
+			form = UploadFileFrom()
+			return render_to_response('archivo.html',{'form': form})

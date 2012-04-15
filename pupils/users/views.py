fd462e48693a4995802357570c280db54fc97eb3
franlu <franlu@58014b41-5bfd-43f5-a4be-6ab8f3083e9b>
2012-04-15 15:06:37 +0000

    [UP] db con nuevos usuarios
    El padre puede inscribir a 'n' hijos

--- pupils/users/views.py
+++ pupils/users/views.py
@@ -7,8 +7,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django import forms
 
+from registration import signals
 from forms import HijoExtraForm
-from users.models import Padre
+from users.models import Padre, Hijo
 
 @login_required
 def panel(request):
@@ -51,20 +52,24 @@ def inscribir_hijo(request):
 		forms = HijoExtraForm(request.POST, request.FILES)
 		
 		if forms.is_valid():
-			padre = Padre.objects.get(id=request.user.id)
-			print padre.user.username
 			new_user = User()
 			new_user.password = request.POST['password1']
 			new_user.username = request.POST['username']
+			new_user.first_name = request.POST['name']
+			new_user.last_name = request.POST['surname']
 			new_user.email = request.POST['email']
 			new_user.activate = True
-			#new_user.save()
-			custom = Hijo(user=new_user)
-			custom.activity_id = padre.activity_id
-			custom.year = request.POST['year']
-			custom.observations = request.POST['observations']
-			#custom.save()
-			#padre.children 
+			new_user.save()
+			
+			hijo = Hijo(user=new_user)
+			padre = Padre.objects.get(user=request.user)
+			hijo.activity_id = padre.activity_id
+			hijo.year = request.POST['year']
+			hijo.observations = request.POST['observations']
+			hijo.save()
+			padre.children.add(hijo)
+			padre.save()
+			
 			return HttpResponseRedirect('/pcontrol/')
 		else:
 			context= {
